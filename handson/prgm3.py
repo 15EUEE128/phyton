f80330776d7fc571bb5fa9fbcb0d00d64ac9cb9d
SARANYA <34641380+15EUEE128@users.noreply.github.com>
2017-12-26 19:31:24 +0530

    Create prgm3.py

---
+++ handson/prgm3.py
@@ -0,0 +1,11 @@
+emp_id=1001
+emp_bs=15000
+emp_al=6000
+gross_salary=emp_bs+emp_al
+print("The gross salary is:",gross_salary)
+if gross_salary>10000:
+	tax=gross_salary*0.2
+	print("tax:",tax)
+net_salary=gross_salary-tax
+print("The net salary is:",net_salary)
+	
